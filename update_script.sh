a9f4153198e46508ce8e4aff0277edc781f6abde
Georg Kettele <kettele.georg@gmail.com>
2024-04-09 13:24:34 +0200

    Update update_script.sh

---
+++ update_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Use curl to check for internet connectivity
+
+if curl -s --head http://www.google.com/ &> /dev/null; then
+  echo "Internet connection detected."
+  sudo rm -rf /home/pi/DavidRamLotusPod/*
+  sudo rm -rf /home/pi/DavidRamLotusPod
+  git clone https://github.com/pixelcrash/DavidRamLotusPod.git
+ # sudo rm -rf /home/pi/flask/*
+  sudo cp -rf /home/pi/DavidRamLotusPod/* /home/pi/flask
+  # Place your commands here that should run when there is an Internet connection
+  
+else
+  echo "No Internet connection."
+  # Place your commands here that should run when there is no Internet connection
+fi
+
+sudo reboot -h now
